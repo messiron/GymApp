77bbb152e1bc9d3300ab2421cb4a097761068262
Juan Sebastian Morales Cardona <sebasmorales05col@gmail.com>
2026-04-02 23:46:09 -0500

    feat/auth: config user module and change interface by abstract class

--- backend/src/modules/user/core/ports/output/user-repository.port.ts
+++ backend/src/modules/user/core/ports/output/user-repository.port.ts
@@ -1,9 +1,9 @@
 import { User } from "../../entities/user.entity";
 
-export interface UserRepositoryPort {
-  findById(id: string): Promise<User | null>;
-  findByEmail(email: string): Promise<User | null>;
-  create(id: string, email: string): Promise<void>;
-  update(user: User): Promise<void>;
-  delete(id: string): Promise<void>;
+export abstract class UserRepositoryPort {
+  abstract findById(id: string): Promise<User | null>;
+  abstract findByEmail(email: string): Promise<User | null>;
+  abstract create(id: string, email: string): Promise<void>;
+  abstract update(user: User): Promise<void>;
+  abstract delete(id: string): Promise<void>;
 }
--- backend/src/modules/user/user.module.ts
+++ backend/src/modules/user/user.module.ts
@@ -1,4 +1,25 @@
 import { Module } from '@nestjs/common';
+import { CreateUserUseCase } from './core/use-cases/create-user.use-case';
+import { FindUserUseCase } from './core/use-cases/find-user.use-case';
+import { FindUserByEmailUseCase } from './core/use-cases/find-user-by-email.use-case';
+import { UserRepositoryPort } from './core/ports/output/user-repository.port';
+import { PrismaUserRepository } from './infrastructure/repositories/prisma-user.repository';
 
-@Module({})
+@Module({
+  imports: [],
+  providers: [
+    CreateUserUseCase,
+    FindUserUseCase,
+    FindUserByEmailUseCase,
+    {
+      provide: UserRepositoryPort,
+      useClass: PrismaUserRepository
+    }
+  ],
+  exports: [
+    CreateUserUseCase,
+    FindUserUseCase,
+    FindUserByEmailUseCase,
+  ]
+})
 export class UserModule {}
